539900840e24ceccf4b337ef8fe3ffc7b1270765
Federico Collasius <fede.collasius@gmail.com>
2025-12-11 19:08:07 -0300

    feat: finish day3 2022

--- 2022/day3/Day3.scala
+++ 2022/day3/Day3.scala
@@ -1,4 +1,5 @@
-scala.io.Source.fromFile("input.txt").getLines().toList
+/*
+val part1 = scala.io.Source.fromFile("input.txt").getLines().toList
   .map{ l => 
     val length = l.length
     val (fst, snd) = l.splitAt(length/2)
@@ -12,5 +13,23 @@ scala.io.Source.fromFile("input.txt").getLines().toList
       char.toInt - 38
   }
   .sum
-
-
+*/
+val part2 = scala.io.Source.fromFile("input.txt")
+  .getLines()
+  .toList
+  .grouped(3)
+  .toList
+  .map{ group => 
+   group match {
+      case List(elf1, elf2, elf3) => 
+        elf1.intersect(elf2).intersect(elf3)
+    }
+  }
+  .map{ str => 
+    val char = str.head
+    if (char.isLower)
+      char.toInt - 96
+    else 
+      char.toInt - 38
+  }
+  .sum
