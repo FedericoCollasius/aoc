d933a9ebb585b14d31d171d3d402fee2533cf828
Federico Collasius <fede.collasius@gmail.com>
2025-12-14 22:13:57 -0300

    feat: day5-2022 done

--- 2022/day5/Day5.scala
+++ 2022/day5/Day5.scala
@@ -1,19 +1,36 @@
-val input = scala.io.Source.fromFile("sample.txt").mkString.split("\n\n")
+val input = scala.io.Source.fromFile("input.txt").mkString.split("\n\n")
 
-val numStacks = input(0).takeRight(2).trim.toInt
-val buckets: Vector[List[Char]] = Vector.fill(numStacks)(List())
-val stacks= input(0).split("\n")
-  .dropRight(1)
-  .flatMap{line =>
+//val numStacks = input(0).takeRight(2).trim.toInt
+//val buckets: Vector[List[Char]] = Vector.fill(numStacks)(List())
+var stacks= input(0).split("\n").dropRight(1).flatMap{line =>
     line.grouped(4).toList.zipWithIndex.collect{
       case(str, idx) if str.trim.nonEmpty && str.contains('[') =>
         (idx, str.trim.replaceAll("\\[|\\]", "").head)
     }
-  }
-  .groupMap(_._1)(_._2)
-  .toVector
-  .map(_._2.toList)
+  }.groupMap(_._1)(_._2).toVector.sortBy(_._1).map(_._2.toList)
 
+val part1 = input(1).split("\n")
+  .map{ lines =>
+    lines match {
+      case s"move $nStr from $fromStr to $toStr" =>
+        val n = nStr.toInt
+        val from = fromStr.toInt - 1
+        val to = toStr.toInt - 1
+        val newStackTo = stacks(from).take(n) ::: stacks(to)
+        val newStackFrom = stacks(from).drop(n)
+        stacks = stacks
+          .updated(from, newStackFrom)
+          .updated(to, newStackTo)
+        println(s"$stacks")
+    }
+  }
+  // Maybe it would have made more sense to use a foldLeft
+  // Also a List of char is a string basically
+  // So a Vector[String] would have been simpler
+
+stacks.map(_.head).toString
+
+//First try:
 // val stacksToParse = input(0).split("\n")
 //   .dropRight(numStacks*2 + 1)
 //   .map(_.grouped(4).toList)
@@ -24,3 +41,12 @@ val stacks= input(0).split("\n")
 //     if (!cleanStr.isEmpty) cleanStr::buckets(idx)    
 //   }
 // val stacks = buckets.map(_.reverse)
+
+// Much cleaner parsing:
+// val lines = io.Source.fromResource("2022/day-05.txt").getLines()
+// val drawing = lines.takeWhile(_.nonEmpty).toVector
+// val steps = lines.toList
+//
+// val start: State = drawing.init.transpose.collect {
+//   case col if col.exists(_.isLetter) => col.mkString.stripLeading
+// }
